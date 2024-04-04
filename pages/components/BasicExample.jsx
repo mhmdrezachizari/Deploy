b892390b92409a9fea7ff899fec733283d864989
unknown <mhmdrezachizari@gmail.com>
2024-04-04 21:52:11 +0330

    salam

---
+++ pages/components/BasicExample.jsx
@@ -0,0 +1,25 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Link from 'next/link'
+
+function BasicExample() {
+  return (
+    <Navbar expand="lg" className="bg-dark">
+      <Container>
+      <Link href='/about' className='nav-link navbar-brand text-light'>mhmdrezachizari</Link>
+        <Navbar.Toggle  className='text-bg-danger ' claria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Link href='/product' className='nav-link text-light'>Products</Link>
+            <Link href='/sale' className='nav-link text-light'>Increadible offer</Link>
+            <Link href='/Login' className='nav-link text-light'>Login</Link>
+            <Link href='/about' className='nav-link text-light'>about</Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample;
