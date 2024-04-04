3c84fb8ba81083b5a37ef4ee0ac6a8564a7cbdf5
unknown <mhmdrezachizari@gmail.com>
2024-04-04 22:41:37 +0330

    salam

--- pages/components/BasicExample.jsx
+++ pages/components/BasicExample.jsx
@@ -2,19 +2,20 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Link from 'next/link'
+import Styles from './nabar.module.css'
 
 function BasicExample() {
   return (
     <Navbar expand="lg" className="bg-dark">
       <Container>
-      <Link href='/about' className='nav-link navbar-brand text-light'>mhmdrezachizari</Link>
-        <Navbar.Toggle  className='text-bg-danger ' claria-controls="basic-navbar-nav" />
+      <Link href='/about' className={Styles.navbarbrand}>mhmdrezachizari</Link>
+        <Navbar.Toggle  className={Styles.c1} claria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Link href='/Login' className='nav-link text-light'>Login</Link>
-            <Link href='/product' className='nav-link text-light'>Products</Link>
-            <Link href='/sale' className='nav-link text-light'>Increadible offer</Link>
-            <Link href='/about' className='nav-link text-light'>about</Link>
+            <Link href='/Login' className={Styles.linkbav}>Login</Link>
+            <Link href='/product' className={Styles.linkbav}>Products</Link>
+            <Link href='/sale' className={Styles.linkbav}>Increadible</Link>
+            <Link href='/about' className={Styles.linkbav}>about</Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
